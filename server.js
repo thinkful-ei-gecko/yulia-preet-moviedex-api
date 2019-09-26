9df5ae405f0383f8fc6a5c0b69ee39988628ef95
preet-singh <preetsingh13@gmail.com>
2019-09-26 11:28:52 -0500

    corrections made to filters

--- server.js
+++ server.js
@@ -25,28 +25,25 @@ app.use(function validateBearerToken(req, res, next){
 //function for requesting the movies by user query
 function handleGetMovies(req, res) {
   const { genre, country, avg_vote} = req.query;
-  if(!genre && !country && !avg_vote) {
-    res.status(400).json({error: 'Must provide a search query'});
-  }
+  let result = movies;
+
   if (genre ) {
-    let movieByGenre = movies.filter( movie =>
+    result = result.filter( movie =>
       movie.genre.toLowerCase().includes(genre.toLowerCase())
     );
-    res.status(200).json(movieByGenre);
   }
   if (country) {
-    let movieByCountry = movies.filter( movie => 
+    result = result.filter( movie => 
       movie.country.toLowerCase().includes(country.toLowerCase())
     );
-    res.status(200).json(movieByCountry);
   }
   if (avg_vote) {
-    let movieByVote = movies.filter( movie => {
+    result = result.filter( movie => {
       const avg = parseFloat(avg_vote);
       return parseFloat(movie.avg_vote) >= avg;
     });
-    res.status(200).json(movieByVote);
   }
+  return res.status(200).json(result);
 }
 
 app.get('/movies', handleGetMovies);
